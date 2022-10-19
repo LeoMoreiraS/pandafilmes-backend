d7dc05e75cd93d6e55bd80075a9d82d4de117f90
Ian Langkammer Batista <ilangkammer16@gmail.com>
2022-10-19 20:02:59 -0300

    test: add singleton test and coverage gathering

--- src/services/__tests__/DeleteVideoService.spec.ts
+++ src/services/__tests__/DeleteVideoService.spec.ts
@@ -1,37 +1,39 @@
 import { DeleteVideosService } from "../DeleteVideosService";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-describe('Delete Videos Service Test', () => { 
-    const deleteVideosService = DeleteVideosService.getInstance();
+describe("Delete Videos Service Test", () => {
+  const deleteVideosService = DeleteVideosService.getInstance();
 
-    it("Should not try to delete a file that does not exist",()=>{
+  it("Should create only 1 instance of the service", () => {
+    const uploadVideosService2 = DeleteVideosService.getInstance();
+    expect(deleteVideosService).toBe(uploadVideosService2);
+  });
 
-        try{
-            deleteVideosService.execute('invalidFile.file');
-            expect(true).toBeFalsy();
-        }catch(error){
-            if(error instanceof Error){
-                expect(error.message).toBe("No such file");
-            }else{
-                expect(true).toBeFalsy();
-            }
-        }
-    });
+  it("Should not try to delete a file that does not exist", () => {
+    try {
+      deleteVideosService.execute("invalidFile.file");
+      expect(true).toBeFalsy();
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).toBe("No such file");
+      } else {
+        expect(true).toBeFalsy();
+      }
+    }
+  });
 
-    it("Should delete a valid video and thumbnail",()=>{
+  it("Should delete a valid video and thumbnail", () => {
+    const testFilePath = path.resolve("videos", "test1.mp4");
+    const testThumbPath = path.resolve("videos", "test1.jpg");
+    fs.writeFileSync(testFilePath, "123");
+    fs.writeFileSync(testThumbPath, "123");
 
-        const testFilePath = path.resolve("videos","test1.mp4"); 
-        const testThumbPath = path.resolve("videos", "test1.jpg"); 
-        fs.writeFileSync(testFilePath, "123");
-        fs.writeFileSync(testThumbPath, "123");
+    const videoPath = path.resolve("videos", "test1.mp4");
+    const thumbPath = path.resolve("videos", "test1.jpg");
 
-        const videoPath = path.resolve("videos","test1.mp4");
-        const thumbPath = path.resolve("videos","test1.jpg");
-        
-        deleteVideosService.execute("test1.mp4");
-        expect(fs.existsSync(videoPath)).toBeFalsy();
-        expect(fs.existsSync(thumbPath)).toBeFalsy();
-    })
-
- })
+    deleteVideosService.execute("test1.mp4");
+    expect(fs.existsSync(videoPath)).toBeFalsy();
+    expect(fs.existsSync(thumbPath)).toBeFalsy();
+  });
+});
